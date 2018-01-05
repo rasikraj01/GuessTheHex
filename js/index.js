905ac3a07ed9536e9cb06186bcc4b5b6279c0fe2
Rasik <rasikraj01@gmail.com>
2018-01-05 20:25:53 +0530

    updates

--- js/index.js
+++ js/index.js
@@ -1,47 +1,57 @@
-function ran() {
-      return Math.floor((Math.random() * 16) + 1);
-}
-
-// var hex = `${ran().toString(16)} + ${ran().toString(16)} + ${ran().toString(16)} + ${ran().toString(16)} + ${ran().toString(16)} + ${ran().toString(16)}`;
-
-
-
-function genhex() {
-   var hex1 = ran().toString(16);
-   var hex2 = ran().toString(16);
-   var hex3 = ran().toString(16);
-   var hex4 = ran().toString(16);
-   var hex5 = ran().toString(16);
-   var hex6 = ran().toString(16);
-
-   var hex = hex1 + hex2 + hex3 + hex5 + hex4 + hex6;
-   return hex = hex.toUpperCase().slice(0,6);
-}
-
-hex_q = genhex();
-
-$('h2').append(`#${hex_q}`);
-var html = '';
-var pos = Math.floor(Math.random() * 5);
-for (var i = 0; i < 5; i++) {
-   var hex_o = genhex();
-   if (i === pos) {
-      hex_o = hex_q;
+function newHEX() {
+   function ran() {
+         return Math.floor((Math.random() * 16) + 1);
    }
-   html += `<li id="${hex_o}" style = "background:#${hex_o}"></li>`;
+
+   // var hex = `${ran().toString(16)} + ${ran().toString(16)} + ${ran().toString(16)} + ${ran().toString(16)} + ${ran().toString(16)} + ${ran().toString(16)}`;
+
+   function genhex() {
+      var hex1 = ran().toString(16);
+      var hex2 = ran().toString(16);
+      var hex3 = ran().toString(16);
+      var hex4 = ran().toString(16);
+      var hex5 = ran().toString(16);
+      var hex6 = ran().toString(16);
+
+      var hex = hex1 + hex2 + hex3 + hex5 + hex4 + hex6;
+      return hex = hex.toUpperCase().slice(0,6);
+   }
+
+   hex_q = genhex();
+   console.log(hex_q);
+   $('h2').html(`#${hex_q}`);
+   var html = '';
+   var pos = Math.floor(Math.random() * 5);
+   for (var i = 0; i < 5; i++) {
+      var hex_o = genhex();
+      if (i === pos) {
+         hex_o = hex_q;
+      }
+      html += `<li id="${hex_o}" style = "background:#${hex_o}"></li>`;
+   }
+   //console.log(html);
+   $('.options ul').html(html);
 }
-//console.log(html);
-$('.options ul').append(html);
-
-var flag = 0;
-
+newHEX();
+var score = 0;
+$('.currentScore p').html(score);
 $('li').click(function (){
    var color = this.id;
    if(color === hex_q){
-      $('.result h4').append('CORRECT!').setTimeut(200 , reload());
+      score++;
+      $('.currentScore p').html(score);
+      $('.result h4').append('CORRECT!');
+      newHEX();
+      //.setTimeout(location.reload(), 3000)
    }
    else{
-      console.log(false);
-   }
+      var lose = `<span style="color: red ; font-weight:300;"> YOU LOSE !</span><div class="newgame">New Game</div>`
+      $('.result h4').append(lose);
 
+      $('.newgame').click(function () {
+         console.log('ssss');
+         newHEX();
+         score = 0;
+      });
+   }
 });
